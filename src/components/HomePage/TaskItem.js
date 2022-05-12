669a607cbbf71e1c8b2e52c2968bd3cfb270ef4c
Aryan Sharma <arubk744@gmail.com>
2022-05-12 20:36:31 +0530

    animated checkbox

--- src/components/HomePage/TaskItem.js
+++ src/components/HomePage/TaskItem.js
@@ -5,6 +5,7 @@ import { useDispatch } from 'react-redux';
 import { deleteItem, checkItem } from './../../redux/userData';
 
 import { motion, AnimatePresence } from 'framer-motion';
+import CheckBox from 'react-animated-checkbox';
 
 const TaskItem = ({ checked, title, id }) => {
   const dispatch = useDispatch();
@@ -16,16 +17,37 @@ const TaskItem = ({ checked, title, id }) => {
       exit={{ x: -150, opacity: 0 }}
       className='flex flex-row items-center justify-between px-4 py-2 my-2 bg-white text-gray-800 dark:bg-gray-900 dark:text-white border-2 border-blue-400 rounded-xl text-md '>
       <div className='flex flex-row items-center '>
-        <button
+        {/* <button
           onClick={() => dispatch(checkItem(id))}
           className='text-3xl active:scale-[0.98] mr-2 transition-all transition-duration-[300ms] text-blue-500 '>
           {checked ? <IoMdRadioButtonOn /> : <IoMdRadioButtonOff />}
-        </button>
-        <p>{title}</p>
+        </button> */}
+
+        <div className='flex mr-2'>
+          <CheckBox
+            checked={checked}
+            checkBoxStyle={{
+              checkedColor: '#3498DB',
+              size: 20,
+              unCheckedColor: '#b8b8b8',
+            }}
+            duration={200}
+            onClick={() => dispatch(checkItem(id))}
+          />
+        </div>
+
+        {/* <button
+          onClick={() => dispatch(checkItem(id))}
+          className='text-3xl active:scale-[0.98] mr-2 transition-all transition-duration-[300ms] text-blue-500 '>
+          {checked ? <IoMdRadioButtonOn /> : <IoMdRadioButtonOff />}
+        </button> */}
+        <p className={`${checked ? ' text-gray-500 line-through' : ''}`}>
+          {title}
+        </p>
       </div>
       <button
         onClick={() => dispatch(deleteItem(id))}
-        className='p-2 ml-2 text-lg text-white transition-all bg-red-400 rounded-lg active:scale-95 align-self-end '>
+        className='p-2 ml-2 text-lg text-white  transition-all bg-red-400 rounded-lg  active:scale-95 align-self-end '>
         <AiOutlineDelete />
       </button>
     </motion.div>
